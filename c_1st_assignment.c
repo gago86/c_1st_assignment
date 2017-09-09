2f2307f204d5555c0d22c6d6889a4c2b00c12fa5
gago86 <31676006+gago86@users.noreply.github.com>
2017-09-10 02:15:38 +0900

    Add files via upload

---
+++ c_1st_assignment.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+	printf("My student number : 2017301057\n");
+	printf("My name : Lee Seung-chul\n");
+	printf("My phone number : 01020849572\n");
+	printf("my github address : gago86@naver.com\n");
+
+	return 0;
+	}
